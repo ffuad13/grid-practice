aa274bedb63f54d219c927096981a817a232db7e
ffuad13 <jhonatom99@gmail.com>
2021-10-05 00:24:19 +0800

    add switch theme logic

--- script/script.js
+++ script/script.js
@@ -1,6 +1,17 @@
+/* Menu toggle */
 const menuToggle = document.querySelector(".menu-toggle");
 const nav = document.querySelector("nav ul");
 
 menuToggle.addEventListener("click", function () {
 	nav.classList.toggle("slide");
+});
+
+/* color theme */
+let theme = document.getElementsByClassName("theme")[0];
+document.getElementById("toggler").checked = false;
+
+document.getElementById("toggler").addEventListener("change", (event) => {
+	event.target.checked
+		? theme.setAttribute("data-theme", "dark")
+		: theme.removeAttribute("data-theme");
 });
